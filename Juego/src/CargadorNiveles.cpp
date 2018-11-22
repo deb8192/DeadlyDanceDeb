19cd881f4a357215138b99835e5ec6d168d3d657
JoseVicente <josevicenteantoncoy@hotmail.com>
2018-11-22 14:23:31 +0100

    Modificaciones finales para cargador de nivel basico (Faltara un poco de implementacion para las diferentes opciones que necesitamos)

--- Juego/src/CargadorNiveles.cpp
+++ Juego/src/CargadorNiveles.cpp
@@ -35,23 +35,42 @@ void CargadorNiveles::CargarNivelXml(int level)
         int x = hijo.attribute("X").as_int();//nos devuelve un int
         int y = hijo.attribute("Y").as_int();//nos devuelve un int
         int z = hijo.attribute("Z").as_int();//nos devuelve un int 
+        bool jugadorEstasAqui = hijo.attribute("UserStar").as_bool();//nos devuelve true si es donde empieza el jugador 
         const char * textura = hijo.attribute("Texture").value(); //nos da un char[] = string
         const char * modelo  =  hijo.attribute("Model").value(); //nos da un char[] = string
         motor->CargarPlataformas(x,y,z,modelo,textura); //cargamos el objeto
-
-        //carga del jugador
-        //if(estajugador){}
+        
+        if(jugadorEstasAqui)
+        {
+            //cogemos las posiciones del jugador en el escenario
+            int Playerx = hijo.attribute("StarX").as_int();//nos devuelve un int
+            int Playery = hijo.attribute("StarY").as_int();//nos devuelve un int
+            int Playerz = hijo.attribute("StarZ").as_int();//nos devuelve un int 
+            const char * Playertextura = hijo.attribute("StarTexture").value(); //nos da un char[] = string
+            const char * Playermodelo  =  hijo.attribute("StarModel").value(); //nos da un char[] = string
+            motor->CargarJugador(Playerx,Playerz,Playery,Playermodelo,Playertextura);
+        }
 
         for (pugi::xml_node enem = hijo.child("Enemy"); enem; enem = enem.next_sibling("Enemy"))//esto nos devuelve todos los hijos que esten al nivel del anterior
         {
             //aqui va la carga de enemigos
-            //motor->CargarEnemigos(x,y,z,modelo,textura); //cargamos el enemigo
+            int x = enem.attribute("X").as_int();//nos devuelve un int
+            int y = enem.attribute("Y").as_int();//nos devuelve un int
+            int z = enem.attribute("Z").as_int();//nos devuelve un int 
+            const char * textura = enem.attribute("Texture").value(); //nos da un char[] = string
+            const char * modelo  =  enem.attribute("Model").value(); //nos da un char[] = string
+            motor->CargarEnemigos(x,z,y,modelo,textura); //cargamos el enemigo
         }
 
         for (pugi::xml_node obj = hijo.child("Object"); obj; obj = obj.next_sibling("Object"))//esto nos devuelve todos los hijos que esten al nivel del anterior
         {
             //aqui va la carga de objetos
-            //motor->CargarObjetos(x,y,z,modelo,textura); //cargamos el enemigo
+            int x = obj.attribute("X").as_int();//nos devuelve un int
+            int y = obj.attribute("Y").as_int();//nos devuelve un int
+            int z = obj.attribute("Z").as_int();//nos devuelve un int 
+            const char * textura = obj.attribute("Texture").value(); //nos da un char[] = string
+            const char * modelo  =  obj.attribute("Model").value(); //nos da un char[] = string
+            motor->CargarObjetos(x,z,y,modelo,textura); //cargamos el enemigo
         }
     }
 
