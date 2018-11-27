fea211f810ae53ed7757557ab3dc880e9ca3e429
davidramons <davidramonsevilla@gmail.com>
2018-11-27 21:09:54 +0100

    mejorado motor audio

--- Juego/src/MotorAudio.hpp
+++ Juego/src/MotorAudio.hpp
@@ -6,7 +6,7 @@
 class MotorAudio
 {
   //Puntero de instancia a FMOD
-  FMOD::System *m_pSystem;
+  FMOD::System *pSystem;
   MotorAudio(){}
 }
 
@@ -16,19 +16,51 @@ typedef FMOD::Sound* ClaseSonido;
 MotorAudio()
 {
   //Si ya existe el sistema, error
-  if(FMOD::System_Create(&m_pSystem) != FMOD_OK)
+  if(FMOD::System_Create(&pSystem) != FMOD_OK)
   {
     return; //error
   }
 
-  //Contar los
+  //Si el PC tiene algun driver de sonido
   int driverCount = 0;
-  m_pSystem->getNumDrivers(&driverCount)
+  pSystem->getNumDrivers(&driverCount)
 
-  if/driverCount == 0)
+  //Si el PC no tiene driver de sonido
+  if(driverCount == 0)
   {
     return; //Error
   }
+
+  //Inicializar la instancia con 36 canales
+  pSystem->init(36, FMOD_INIT_NORMAL, NULL);
 }
 
+//Crear sonido (puntero de ClaseSonido, ruta del archivo)
+void crearSonido(ClaseSonido *pSound, const char* pFile)
+ {
+    pSystem->crearSonido(pFile, FMOD_HARDWARE, 0, pSound);
+ }
+
+//Reproducir sonido (sonido, sonido en bucle o no)
+ void playSonido(ClaseSonido pSound, bool bucle = false)
+  {
+     //Sonido 1 vez
+     if (!bucle)
+        pSound->setMode(FMOD_LOOP_OFF);
+     else
+     //Sonido en bucle
+     {
+        pSound->setMode(FMOD_LOOP_NORMAL);
+        pSound->setLoopCount(-1); //Contador de bucles ilimitado
+     }
+
+     //Sonido en un canal libre
+     m_pSystem->playSound(FMOD_CHANNEL_FREE, pSound, false, 0);
+  }
+
+  //Liberar el sonido
+  void liberarSonido(SoundClass pSound)
+  {
+    pSound->release();
+  }
 #endif
