6815a68f4725942601fd9cbb0669284f5813c752
JoseVicente <josevicenteantoncoy@hotmail.com>
2018-11-28 01:14:46 +0100

    Se crea primera version solo puede tener una entrada y una salida (se creara para que pueda tener multiples entradas), es funcional ya faltaria definir los tamaños y recogerlos.

--- Juego/src/CargadorNiveles.hpp
+++ Juego/src/CargadorNiveles.hpp
@@ -16,9 +16,9 @@ class CargadorNiveles
         void GuardarNivelXml(int); //esta funcion guarda el nivel en xml
         void GuardarNivelBin(int); //esta funcion guarda el nivel en formato bin con el numero que le pases
         void CargarNivelBin(int); //esta funcion carga el nivel desde un archivo binario  
-
-    private:
         
+    private:
+        Sala * crearSala(pugi::xml_node hijo,Sala * padre);//se llama recursivamente si hay salas
         int * lista;
 };
 
