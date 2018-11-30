0b4e126569203509b4f1ee30b60f2e49eb53f175
davidramons <davidramonsevilla@gmail.com>
2018-11-30 17:17:50 +0100

    Ya ejecuta musica de fondo

--- Juego/src/MotorAudio.hpp
+++ Juego/src/MotorAudio.hpp
@@ -10,50 +10,27 @@
 typedef FMOD::Sound* ClaseSonido;
 
 //Clase motor audio
-class MotorAudio
+struct MotorAudio
 {
-  FMOD::Studio::System* pSystem = NULL;  //Puntero de instancia a FMOD
-  FMOD::System *plSystem = NULL;         //Puntero de instancia low level
-
-  //Comprobar errores en FMOD
-  void ERRCHECK(FMOD_RESULT result)
-  {
-    if (result != FMOD_OK){
-       printf("FMOD error! (%d) %s\n", result, FMOD_ErrorString(result));
-       exit(-1);
+    ~MotorAudio(void);
+    static MotorAudio *getInstance()
+    {
+        if(maudio_instancia == 0)
+        maudio_instancia = new MotorAudio();
+        return maudio_instancia;
     }
-  }
+    void createSound(ClaseSonido *pSound, const char* pFile);   //Crear sonido
+    void playSound(ClaseSonido pSound);                         //Ejecutar sonido
+    void releaseSound(ClaseSonido pSound);                      //Liberar sonido
 
-public:
-  //Constructor
-  MotorAudio()
-  {
-    //Creamos el sistema
-    ERRCHECK(FMOD::Studio::System::create(&pSystem));
+  private:
 
-    //Obtener LOW-level system
-    ERRCHECK(pSystem->getLowLevelSystem(&plSystem));
-    ERRCHECK(plSystem->setSoftwareFormat(0,FMOD_SPEAKERMODE_5POINT1, 0));
-    ERRCHECK(plSystem->setOutput(FMOD_OUTPUTTYPE_AUTODETECT));
+    FMOD::Studio::System* pSystem;      //Puntero de instancia a FMOD
+    FMOD::System *plSystem;             //Puntero de instancia low level
+    void ERRCHECK(FMOD_RESULT result);  //Comprobar errores en FMOD
+    MotorAudio();                       //Singleton
+    static MotorAudio* maudio_instancia;
 
-    //Inicializar la instancia con x canales
-    ERRCHECK(pSystem->initialize(1024, FMOD_STUDIO_INIT_NORMAL, FMOD_INIT_NORMAL, 0));
-  }
-
-  void createSound(ClaseSonido *pSound, const char* pFile)
-  {
-     ERRCHECK(plSystem->createSound(pFile, FMOD_3D, 0, pSound));
-  }
-
-  void playSound(ClaseSonido pSound)
-  {
-     ERRCHECK(plSystem->playSound(pSound, 0, false, 0));
-  }
-
-  void releaseSound(ClaseSonido pSound)
-  {
-     pSound->release();
-  }
 
 };
 
--- Juego/src/main.cpp
+++ Juego/src/main.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
 #include "Juego.hpp"
-
+#include "MotorAudio.hpp"
 int main()
 {
 	cout << "\e[42m Iniciando Juego \e[0m" << endl;
 	Juego *juego = Juego::getInstance();
 	juego->InicializarVentana();
+
+	// Sonido systema
+	MotorAudio* sound = MotorAudio::getInstance();  //se recoge instancia de motor
+	// Crear sonido
+	ClaseSonido soundSample;
+	sound->createSound(&soundSample, "assets/sounds/01.mp3");
+	// Ejecutar sonido
+	sound->playSound(soundSample);
+
 	while(juego->Running())//comprobamos que esta activo el dispositivo
 	{
 		juego->Update();//si lo esta actualizamos la escena
