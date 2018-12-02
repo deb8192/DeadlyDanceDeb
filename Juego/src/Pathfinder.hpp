670d5d50a8aeb08ef96ccba88b7444dc6e4ec07f
deb8192 <dgg47@alu.ua.es>
2018-12-02 14:29:16 +0100

    Pathfinding terminado a falta de pruebas

---
+++ Juego/src/Pathfinder.hpp
@@ -0,0 +1,35 @@
+#ifndef Pathfinder_HPP
+#define Pathfinder_HPP
+#include <string>
+#include <cstring>
+#include <vector>
+#include <iostream>
+#include <cmath>
+#include<bits/stdc++.h> 
+#include "Nivel.hpp"
+#include "Sala.hpp"
+
+using namespace std;
+
+class Pathfinder
+{
+    public:
+        struct NodeRecord;
+        int comprobarListas(Sala*, vector <struct NodeRecord> lista);
+        int  calcularCostes(struct NodeRecord, Sala*);
+        bool coincide(Sala*, Sala*);
+        vector <struct NodeRecord> encontrarCamino(Nivel, Sala*, Sala*, float);
+        void getSmallest();
+    private:
+        struct NodeRecord{
+            Sala* nodo;
+            struct NodeRecord *conexion;
+            int costSoFar;
+            int estimatedTotalCost;
+        } startNodo, actualNodo, endNodo;
+
+        vector <struct NodeRecord> listaAbierta;
+        vector <struct NodeRecord> listaCerrada;
+        vector <struct NodeRecord> camino;
+};
+#endif
