572b6b56e75e2a1b6274606977a61964b026ff1a
davidramons <davidramonsevilla@gmail.com>
2018-12-05 00:16:59 +0100

    arreglos

--- Juego/src/MotorAudio.cpp
+++ Juego/src/MotorAudio.cpp
@@ -28,16 +28,28 @@ MotorAudioSystem::MotorAudioSystem()
   //Cargar eventos
   FMOD::Studio::EventDescription* pEventInstance;
   ERRCHECK(pstudioSystem->getEvent("event:/Music/Level 01", &pEventInstance));
-  if(pEventInstance)eventDescriptions["Level01"] = pEventInstance; //Insertar en el mapa de descripcion de eventos
+  if(pEventInstance)
+  {
+    eventDescriptions["Level01"] = pEventInstance; //Insertar en el mapa de descripcion de eventos
+    MotorAudioEvents.insert(pair<std::string, MotorAudioEvent*>("Level01",new MotorAudioEvent(pEventInstance)));
+  }
   ERRCHECK(pstudioSystem->getEvent("event:/Music/Level 02", &pEventInstance));
-  if(pEventInstance)eventDescriptions["Level02"] = pEventInstance;
+  if(pEventInstance)
+  {
+    eventDescriptions["Level02"] = pEventInstance; //Insertar en el mapa de descripcion de eventos
+    MotorAudioEvents.insert(pair<std::string, MotorAudioEvent*>("Level02",new MotorAudioEvent(pEventInstance)));
+  }
   ERRCHECK(pstudioSystem->getEvent("event:/Music/Level 03", &pEventInstance));
-  if(pEventInstance)eventDescriptions["Level03"] = pEventInstance;
+  if(pEventInstance)
+  {
+    eventDescriptions["Level03"] = pEventInstance; //Insertar en el mapa de descripcion de eventos
+    MotorAudioEvents.insert(pair<std::string, MotorAudioEvent*>("Level03",new MotorAudioEvent(pEventInstance)));
+  }
 
  //***********EJEMPLO DE EJECUCION MUSICA*****************
-  FMOD::Studio::EventInstance* eventInstance;
-  ERRCHECK(eventDescriptions["Level02"]->createInstance(&eventInstance));
-  ERRCHECK(eventInstance->start());
+  // FMOD::Studio::EventInstance* eventInstance;
+  // ERRCHECK(eventDescriptions["Level02"]->createInstance(&eventInstance));
+  // ERRCHECK(eventInstance->start());
 
 }
 
@@ -48,9 +60,93 @@ MotorAudioSystem::~MotorAudioSystem()
   ERRCHECK(pstudioSystem->release());
 }
 
+MotorAudioEvent* MotorAudioSystem::getEvent(std::string name)
+{
+  map<std::string, MotorAudioEvent*>::iterator motorfound;
+  motorfound = MotorAudioEvents.find(name);
+  return motorfound->second;
+}
+
+//Posicion del punto de escucha
+void MotorAudioSystem::setListenerPosition(float posx, float posy, float posz)
+{
+  FMOD_3D_ATTRIBUTES attributes = {{0}};
+  attributes.forward.z = 1.0f;
+  attributes.up.y = 1.0f;
+  attributes.position.x = posx;
+  attributes.position.y = posy;
+  attributes.position.z = posz;
+  ERRCHECK(pstudioSystem->setListenerAttributes(0, &attributes));
+}
+
 //Actualizar
 void MotorAudioSystem::update(bool paused)
 {
   //Actualizar systema, una vez por frame (si no estas en pausa)
   if(paused == false)ERRCHECK(pstudioSystem->update());
+}
+
+//**********************************************
+//********** MOTOR AUDIO EVENTOS ***************
+//**********************************************
+
+//Constructor
+MotorAudioEvent::MotorAudioEvent(FMOD::Studio::EventDescription* eventdesc)
+{
+  ERRCHECK(eventdesc->createInstance(&soundInstance));
+}
+
+//Destructor
+MotorAudioEvent::~MotorAudioEvent()
+{
+
+}
+
+//Reproducir el evento
+void MotorAudioEvent::start()
+{
+  ERRCHECK(soundInstance->start());
+  //ERRCHECK(soundInstance->setPaused(false));
+}
+
+//Detener el evento
+void MotorAudioEvent::stop()
+{
+    ERRCHECK(soundInstance->stop(FMOD_STUDIO_STOP_IMMEDIATE));
+}
+
+//Pausar el evento
+void MotorAudioEvent::pause()
+{
+  ERRCHECK(soundInstance->setPaused(true));
+}
+
+//Modifica el volumen del evento (entre 0 y 1)
+void MotorAudioEvent::setVolume(float vol)
+{
+  ERRCHECK(soundInstance->setVolume(vol));
+}
+
+//Posicion 3D del evento
+void MotorAudioEvent::setPosition(float posx, float posy, float posz)
+{
+  FMOD_3D_ATTRIBUTES attributes = {{0}};
+  attributes.forward.z = 1.0f;
+  attributes.up.y = 1.0f;
+  attributes.position.x = posx;
+  attributes.position.y = posy;
+  attributes.position.z = posz;
+  ERRCHECK(soundInstance->set3DAttributes(&attributes));
+}
+
+//Si el evento esta sonando
+bool MotorAudioEvent::isPlaying()
+{
+  // FMOD_STUDIO_PLAYBACK_STATE* stateplay = NULL;
+  // ERRCHECK(soundInstance->getPlaybackState(stateplay));
+  // if(*stateplay == FMOD_STUDIO_PLAYBACK_PLAYING)
+  // {
+  //   return true;
+  // }
+   return false;
 }
