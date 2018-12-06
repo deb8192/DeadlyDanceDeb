09b45bbdf6057ff58f4c6438c1066a051f9901d5
dirtdiver353 <dirtdiver353@gmail.com>
2018-12-06 21:29:03 +0100

    Preparado para megear casi
    Codigo casi listo para integrarlo en la rama de desarrollo, unicamente defecto al pulsar dos teclas a la vez

--- Juego/src/Jugador.cpp
+++ Juego/src/Jugador.cpp
@@ -1,5 +1,8 @@
 #include "Jugador.hpp"
 
+#define DEGTORAD 0.0174532925199432957f
+#define RADTODEG 57.295779513082320876f
+
 Jugador::Jugador()
 {
 
@@ -8,9 +11,81 @@ Jugador::Jugador()
 Jugador::Jugador(int,int,int,int,int,int)
 {
     vida = 100;//esto lo hereda de la interfaz por el protected
+    x = 1;
+    z = 20;
 }
+
+float Jugador::getX(){return x;}
+float Jugador::getY(){return y;}
+float Jugador::getZ(){return z;}
+float Jugador::getRX(){return rx;}
+float Jugador::getRY(){return ry;}
+float Jugador::getRZ(){return rz;}
+
 
 void Jugador::AtacarEspecial()
 {
     cout << "Se realiza ataque especial jugador" << endl;
+}
+
+float Jugador::mcd(float ax, float az)
+{
+    return az ? mcd(az, fmod(ax, az)) : ax;
+}
+
+void Jugador::movimiento(float dt,bool a, bool s, bool d, bool w)
+{   
+    cout << a << s << d << w << endl;
+    float px = x,
+          pz = z; 
+    
+    // Comprobar teclas para mover el personaje y la camara
+    if(w)
+    {        
+        //globales usadas para calcular giro, como vector director (x,z)
+        az += 50.0; //cuando mas alto mejor es el efecto de giro
+        az < -1000 ? az = 0 : false;
+        //dir es un vector director usado para incrementar el movimiento
+        pz += 20.0*dt; 
+    }
+    if(s)
+    { 
+        az += -50.0;
+        az > 1000 ? az = 0 : false;
+        pz += -20.0*dt;
+    }
+    if(a)
+    { 
+        ax += -50.0;
+        ax > 1000 ? ax = 0 : false;
+        px += -20.0*dt;
+
+    }
+    if(d)
+    { 
+        ax += 50.0;
+        ax < -1000 ? ax = 0 : false;
+        px += 20.0*dt;
+    }
+    
+    //Para giro: obtienes el maximo comun divisor y lo divides entre x, z
+    //asi evitas que ambas variables aumenten excesivamente de valor
+    float div = mcd(ax,az);
+    
+        if(abs(div) != 1.0)
+        {    
+            ax /= abs(div);
+            az /= abs(div);
+        }
+
+    //esto es para que gire hacia atras ya que al valor que devuelve atan hay que darle la vuelta 180
+    az < 0 ? 
+        deg = 180 + (RADTODEG * atan(ax/az)) :
+        deg =  RADTODEG * atan(ax/az) ;
+
+    //ahora actualizas movimiento y rotacion
+    //has obtenido la arcotangente que te da el angulo de giro en grados            
+    x = px;
+    z = pz;
+    ry = deg;   
 }
--- Juego/src/MotorGrafico.hpp
+++ Juego/src/MotorGrafico.hpp
@@ -5,8 +5,7 @@ cuando este opengl se agregaran mas dependencias. Es una clase singleton (solo h
 #include "eventos.hpp" //este archivo contiene los ids de los eventos
 #include "Inputs.hpp"
 #include <iostream> //la utilizamos para mostrar el log en la consola.
-#include <math.h>
-#include <complex>
+#include <vector>//para los objetos en escena
 
 //para acortar lineas de programacion se cogen espacios definidos directamente
 using namespace irr;
@@ -56,8 +55,13 @@ using namespace idsEventos;
             void borrarScena();//borra todo lo que haya en la ventana
             //borrar gui
             void borrarGui();//borra todo lo que haya en la ventana relacionado con el gui
-            void JointsTest();
-             
+            
+
+
+            /*IMPORTANTE para bullet motor de fisicas y Joints*/
+            //btHingeConstraint(btRigidBody& rbA,const btTransform& rbAFrame, const btTransform& rbBFrame);
+            
+
             //eventos facade
             //detecta si esta pulsado un boton, 1=a, 2 =s, 3=d, 4=w, 5=space, 6=intro 
             bool estaPulsado(int);
@@ -65,12 +69,26 @@ using namespace idsEventos;
             //detecta si un evento con un id pasado a sido llamado
             bool ocurreEvento(int);
             //fin evento botones se puede utilizar para cualquier tipo de evento de irrlicht
-            void movimiento();
-            void closeGame();
-            void setThen();
-            float mcd(float ax, float az);
-            //const f32 getdt();
+            void resetEvento(int);//resetea el evento
             
+            //cargadores de objetos
+            int CargarPlataformas(int x,int y,int z, const char *ruta_objeto, const char *ruta_textura);//carga el objeto en scena lo mete en el array
+            void CargarLuces(int x,int y,int z);
+            void CargarEnemigos(int x,int y,int z, const char *ruta_objeto, const char *ruta_textura);
+            void CargarJugador(int x,int y,int z, const char *ruta_objeto, const char *ruta_textura);
+            void CargarObjetos(int x,int y,int z, const char *ruta_objeto, const char *ruta_textura);
+            
+            void closeGame();
+            
+            //colision rayo
+            bool colisionRayo(int x,int y, int z, int rx, int ry, int rz ,int dimension);
+
+            //debug grafico
+            void activarDebugGrafico();//se activa o desactiva el modo debug grafico
+            void dibujarCirculoEventoSonido(int x, int y, int z, float intensidad);//se dibuja el circulo con la intensidad que se solicita en las coordenadas proporcionadas
+            void clearDebug();//borra toda la informacion de debug en cada update para ver los cambios
+            void dibujarRayo(int x,int y, int z, int rx, int ry, int rz ,int dimension);//dibuja los rayos o lineas con las indicaciones que pongas, (x,y,z) son donde empieza la linea el primer punto, (rx,ry,rz) rotacion de la linea desde su punto, dimension longitud de la linea 
+            void mostrarJugador(float x, float y, float z, float rx, float ry, float rz);
         private: //clases solo accesibles por MotorGrafico
 
             //clase singleton 
@@ -83,22 +101,17 @@ using namespace idsEventos;
             IVideoDriver *driver;
 	        ISceneManager *smgr;
 	        IGUIEnvironment *guienv;
+            scene::ICameraSceneNode* camera;
             IGUIFont *font;
             IGUISkin *skin;
             Inputs input;
-            u32 then;
-            scene::ISceneNode *node;
-            scene::IAnimatedMeshSceneNode* n;
-            scene::ICameraSceneNode* camera;
-            f32 xAxisAngle = 0;
-            f32 zAxisAngle = 0;
-            float dt;
-            bool girar;
-            float newTime, frameTime, interpolation;
-            u32 currentTime;
-            float acumulator;
-            core::vector3df dir, ang;
-            float x, z,deg;   
+            std::vector<IAnimatedMeshSceneNode*> Plataformas_Scena;//plataformas en scena
+            std::vector<ILightSceneNode*> Luces_Scena;//luces en scena
+            std::vector<IAnimatedMeshSceneNode*> Enemigos_Scena;//Enemigos en scena
+            IAnimatedMeshSceneNode *Jugador_Scena;//Jugador en scena
+            std::vector<IAnimatedMeshSceneNode*> Objetos_Scena;//Objetos en scena
+            std::vector<IAnimatedMeshSceneNode*> Objetos_Debug;//Objetos en modo debug
+            bool debugGrafico;//nos sirve para saber si tenemos activado el debug grafico
     };
 
 #endif /* MotorGrafico_HPP */
