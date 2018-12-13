0010362ffa20622e8c73c4033b98e38856f64520
JoseVicente <josevicenteantoncoy@hotmail.com>
2018-12-13 20:36:11 +0100

    Se agrega actualizacion del cuerpo en jugador, se agrega creacion de cuerpo estandar.

--- Juego/src/MotorFisicas.cpp
+++ Juego/src/MotorFisicas.cpp
@@ -8,9 +8,10 @@ MotorFisicas::MotorFisicas()
     config.defaultVelocitySolverNbIterations = 20;
     config.isSleepingEnabled = false;
     space = new CollisionWorld(config);
+    jugador = nullptr;
 }
 
-void MotorFisicas::crearCuerpo(float px, float py, float pz, int type, float ancho, float largo, float alto)
+void MotorFisicas::crearCuerpo(float px, float py, float pz, int type, float ancho, float largo, float alto,int typeCreator)
 {
     rp3d::Vector3 posiciones(px,py,pz);
     rp3d::Quaternion orientacion = rp3d::Quaternion::identity();
@@ -22,21 +23,59 @@ void MotorFisicas::crearCuerpo(float px, float py, float pz, int type, float anc
 
    if(type == 1)//circulo
    {
-            SphereShape * forma = new SphereShape(ancho);
-            cuerpo->addCollisionShape(forma,transformacion);
-            cuerpos.push_back(cuerpo);
+        SphereShape * forma = new SphereShape(ancho);
+        cuerpo->addCollisionShape(forma,transformacion);
     }
     else if(type == 2)//cuadrado (boundingbox)
     {
-            rp3d::Vector3 medidas(ancho,largo,alto);
-            BoxShape * forma = new BoxShape(medidas);
-            cuerpo->addCollisionShape(forma,transformacion);
-            cuerpos.push_back(cuerpo);
+        rp3d::Vector3 medidas(ancho,largo,alto);
+        BoxShape * forma = new BoxShape(medidas);
+        cuerpo->addCollisionShape(forma,transformacion);
     }
     else if(type == 3)//capsula (ideal para personajes)
     {
-            CapsuleShape * forma = new CapsuleShape(ancho,alto);
-            cuerpo->addCollisionShape(forma,transformacion);
-            cuerpos.push_back(cuerpo);
+        CapsuleShape * forma = new CapsuleShape(ancho,alto);
+        cuerpo->addCollisionShape(forma,transformacion);
+    }
+    
+    if(typeCreator == 1)//jugador
+    {
+        jugador = cuerpo;
+    }
+    else if(typeCreator == 2)//enemigos
+    {
+        enemigos.push_back(cuerpo);
+    }
+    else if(typeCreator == 3)//objetos
+    {
+        objetos.push_back(cuerpo);
+    }
+}
+
+Ray * MotorFisicas::crearRayo(float x, float y, float z, float longitud)
+{
+    rp3d::Vector3 inicio(x,y,z);
+    rp3d::Vector3 final(x,y,z*longitud);
+    Ray * rayo = new Ray(inicio,final);
+    return rayo;
+}
+
+void MotorFisicas::colisionRayoUnCuerpo(float x,float y,float z,float longitud)
+{
+    //Ray * rayo = crearRayo(x,y,z,longitud);
+
+    //RaycastInfo intersecion;
+
+    //bool colision = true;
+}
+
+void MotorFisicas::updateJugador(float x, float y, float z, float rx, float ry, float rz)
+{
+    if(jugador != nullptr)
+    {
+        rp3d::Vector3 posiciones(x,y,z);
+        rp3d::Quaternion orientacion = rp3d::Quaternion::identity();
+        Transform transformacion(posiciones,orientacion);
+        jugador->setTransform(transformacion);
     }
 }
--- Juego/src/Nivel.hpp
+++ Juego/src/Nivel.hpp
@@ -5,6 +5,7 @@
 #include "pollo.hpp"
 #include <vector>//para tener los diferentes objetos,enemigos, jugadores.
 #include <ctime>
+#include "MotorFisicas.hpp"
 
 #ifndef Nivel_HPP
 #define Nivel_HPP
@@ -54,6 +55,7 @@ class Nivel
         Jugador jugador;//objeto del jugador en el nivel
         CargadorNiveles cargador;//nos ayuda a cargar los niveles
         Sala * primeraSala;// la primera sala del arbol
+        MotorFisicas *fisicas;//motor de fisicas (hace falta mas descripcion ?)
         int id;//id para las figuras
         float dt;            
         float frameTime;
