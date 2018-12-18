696b3eead40c43999bb03a2db5c5ee7ed7a72e81
deb8192 <dgg47@alu.ua.es>
2018-12-18 14:56:08 +0100

    Ataque especial pruebas

--- Juego/src/Arma.cpp
+++ Juego/src/Arma.cpp
@@ -1,4 +1,9 @@
 #include "Arma.hpp"
+#include <math.h>
+
+
+#define DEGTORAD 0.0174532925199432957f
+#define RADTODEG 57.295779513082320876f
 
 Arma::Arma()
 {
@@ -17,9 +22,29 @@ void Arma::setPosiciones(int nx,int ny,int nz)
     y = ny;
     z = nz;
 }
+void Arma::setPosicionesArmaEsp(float nx,float ny,float nz, float ry)
+{
+    float mx = nx + 6.5*(sin(DEGTORAD*ry));
+    float my = ny;
+    float mz = nz + 6.5*(cos(DEGTORAD*ry));
+
+    this->setPosiciones((int)mx,(int)my,(int)mz);
+}
 int Arma::getAtaque()
 {
     return potenciaAtaque;
+}
+float Arma::getX()
+{
+    return x;
+}
+float Arma::getY()
+{
+    return y;
+}
+float Arma::getZ()
+{
+    return x;
 }
 void Arma::setID(int nid)
 {
