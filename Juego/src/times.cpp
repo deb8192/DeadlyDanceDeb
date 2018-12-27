5afcb1603f9166c257d959e39794be223669de01
JoseVicente <josevicenteantoncoy@hotmail.com>
2018-12-27 14:16:02 +0100

    Se crea funcion para recibir el tiempo actual de ejecucion en milisegundos(1) segundos (2)

--- Juego/src/times.cpp
+++ Juego/src/times.cpp
@@ -103,3 +103,19 @@ float times::getMaximoTiempoFrame()
     float tiempo_restante = (float(tiempo_ahora-tiempoInicio)/(CLOCKS_PER_SEC/1000.0f));
     return (tiempo_frame-tiempo_restante);
 }
+
+float times::getTiempo(int modo)
+{
+    unsigned tiempo_ahora = clock();
+    
+    if(modo == 1)
+    {
+            return (float(tiempo_ahora/(CLOCKS_PER_SEC/1000.0f)));
+    }
+
+    if(modo == 2)
+    {
+            return (float(tiempo_ahora/CLOCKS_PER_SEC));
+    }
+
+}
