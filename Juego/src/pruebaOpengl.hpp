c0ba11815ecbe05ee27168984e66dc94148458c4
josevicenteantoncoy <josevicenteantoncoy@hotmail.com>
2019-01-16 03:42:37 +0100

    Ya funciona opengl, se agrego glew para el funcionamiento aunque tambien esta glad que son para el mismo proposito traducir instrucciones.

--- Juego/src/pruebaOpengl.hpp
+++ Juego/src/pruebaOpengl.hpp
@@ -1,14 +1,21 @@
-#include "glad.h"
+#include "GL/glew.h"
 #include "glfw3.h"
+
+#include <iostream>
 
 #ifndef pruebaOpengl_HPP
 #define pruebaOpengl_HPP
 
-class CargadorNiveles
+class pruebaOpengl
 {
     public:
-
+        pruebaOpengl();
+        void crearVentana();
+        void processInput(GLFWwindow*);
+        void framebuffer_size_callback(GLFWwindow*, int,int);
     private:
+        const unsigned int SCR_WIDTH = 800;
+        const unsigned int SCR_HEIGHT = 600;        
 
 };
 
