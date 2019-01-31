0b7becc3ddd27cdff209891e85486aeb762df39e
JoseVicente <josevicenteantoncoy@hotmail.com>
2019-01-31 23:49:23 +0100

    Primera version arbol (falta mejorarlo y terminar de implementar varias clases a demas falta pintado en preorden)

---
+++ Juego/src/TLuz.hpp
@@ -0,0 +1,25 @@
+#ifndef TLuz_HPP
+#define TLuz_HPP
+
+#include "TEntidad.hpp"
+
+class TLuz : public TEntidad
+{
+    private:
+        float * intensidad;
+
+    public:
+        //constructor y destructor
+        TLuz();
+        ~TLuz();
+        //gestion atributos
+        void setIntensidad(float *);
+        float * getIntensidad();
+        int prueba();
+        
+        //sobrecarga Tentidad
+        void beginDraw();
+        void endDraw();
+};
+
+#endif
---
+++ Juego/src/TNodo.hpp
@@ -0,0 +1,34 @@
+#ifndef TNodo_HPP
+#define TNodo_HPP
+
+#include <vector>
+#include "TEntidad.hpp"
+
+class TNodo
+{
+    private:
+        TEntidad * entidad;//funcion que cumple, si es nullptr es la raiz
+        std::vector<TNodo*> hijos;//array de hijos
+        TNodo * padre;// puntero al TNodo padre
+
+        bool existeHijo(TNodo*);//comprueba si existe el hijo en array de hijos
+
+    public:
+        //destructor y constructor
+        TNodo();
+        ~TNodo();
+
+        //funciones de propiedades
+        int addHijo(TNodo*);//aqui se agrega un hijo al final del vector de hijos se devuelve su posicion
+        int remHijo(TNodo*);//se elimina un hijo del vector de hijos
+        bool setEntidad(TEntidad *);//se comprueba si es una entidad valida para la posicion
+        TNodo * getPadre();//se devuelve el padre
+        void setPadre(TNodo*);//se agrega el padre
+        //pintado
+        void draw();//se llama a las funciones de pintado
+
+};
+
+#endif
+
+
