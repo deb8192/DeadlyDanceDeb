9afcdb09b00d912b1f2e32bc53d83fdb7936ef33
JoseVicente <josevicenteantoncoy@hotmail.com>
2019-02-10 14:04:53 +0100

    Se meten metodos nuevos (ya se puede controlar ventanas normalmente como en irrlicht) He decidido no poner los mismos nombres que en irrlichts puesto que es mas facil entender este motor con nombres en espanol

--- Juego/librerias/catopengl/src/Ventana.cpp
+++ Juego/librerias/catopengl/src/Ventana.cpp
@@ -4,7 +4,7 @@
 Ventana::Ventana()
 {
     std::cout << "inicializar ventana" << std::endl;
-    window = nullptr;
+    _window = nullptr;
 }
 
 //USO: borra los datos que no se borren correctamente por el sistema
@@ -33,48 +33,85 @@ bool Ventana::CrearVentana(int h, int w, bool redimensionar,const char * titulo)
     //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); //Solo para OS X
 
     //Creamos una ventana
-    window = glfwCreateWindow(w, h,titulo, NULL, NULL);  //glfwCreateWindow(ancho,alto,nombre,NULL,NULL)
+    _window = glfwCreateWindow(w, h,titulo, NULL, NULL);  //glfwCreateWindow(ancho,alto,nombre,NULL,NULL)
 
-    if (window == NULL) //si no se crea el objeto ventana, error
+    if (_window == NULL) //si no se crea el objeto ventana, error
     {
         std::cout << "Failed to create GLFW window" << std::endl;
-        glfwTerminate();
+        Drop();
         return false;
     }
 
-    glfwMakeContextCurrent(window);  //Hacer el contexto principal este objeto ventana
+    glfwMakeContextCurrent(_window);  //Hacer el contexto principal este objeto ventana
     //Inicializar GLAD (administra los punteros de OpenGL)
     //Nos da glfwGetProcAddress, donde se define la funcion dependiendo del Sistema operativo al compilar
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
         std::cout << "Failed to initialize GLAD" << std::endl;
-        return -1;
+        return false;
     }
 
     //Viewport, tamanyo de la ventanta de renderizado, para que openGL pueda calcular y mostrar los datos en pantalla (mismo tamanyo que ventana GLFW)
     glViewport(0, 0, w, h);  //glViewport(esq_inf_izquierda_x,esq_inf_izquierda_y,ancho,alto);
 
     //Se llama a la funcion "framebuffer_size_callback" cada vez que el usuario cambia el tamanyo de ventana
-    //glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwSetFramebufferSizeCallback(_window,this->redimensionar);
 
-    //Bucle de render
-    while(!glfwWindowShouldClose(window))    //Comprueba al inicio de la iteracion si se ha ordenado cerrar GLFW, si es asi es TRUE y sale del bucle
-    {
-        //Input
-        //processInput(window);
-
-        //Comandos de render
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f); //Designamos a el glClearColor un color
-        glClear(GL_COLOR_BUFFER_BIT);         //Borrarmos en el buffer con el glClearColor designado
-
-        //Comprobar y llamar eventos, cambiar buffers
-        glfwSwapBuffers(window);             //Intercambia los buffer de la ventana
-        glfwPollEvents();                    //Verifica si se activa algun evento (teclado,raton,etc...)
-    }
-
-    glfwTerminate();   //limpiar todos los recursos en memoria de GLFW
-    
-    std::cout << "valores: " << h << " " << w << "" << titulo << std::endl;
-    
     return true;
+}
+
+//USO: comprueba que esta ventana este abierta
+//SALIDAS: si la ventana esta abierta = true, si esta cerrada = false
+bool Ventana::SigueAbierta()
+{
+    if(!glfwWindowShouldClose(_window))
+    {
+        return true;
+    }
+    return false;
+}
+
+//USO: sirve para limpiar el buffer y cambiar el color ambiente
+void Ventana::draw()
+{
+    //actualizar inputs(teclado y raton)
+    procesarInputs(_window);
+
+    //Comandos de render
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f); //Designamos a el glClearColor un color
+    glClear(GL_COLOR_BUFFER_BIT);         //Borrarmos en el buffer con el glClearColor designado
+
+    //Comprobar y llamar eventos, cambiar buffers
+    glfwSwapBuffers(_window);             //Intercambia los buffer de la ventana
+    glfwPollEvents();  
+}
+
+//USO: sirve para unificar el update general de la ventana(se llaman a las diferentes funciones necesarias para actualizar la ventana)
+void Ventana::Update()
+{
+    draw();//actualizamos la ventana
+}
+
+//USO: limpia toda la informacion de la ventana
+//CUIDADO: esta funcion se llama preferentemente despues de cerrar la ventana.
+void Ventana::Drop()
+{
+    glfwTerminate();//limpiar todos los recursos en memoria de GLFW
+    _window = nullptr;
+}
+
+//USO: se llama automaticamente cuando se redimensiona la pantalla, sirve para redimensionar la pantalla
+//Entradas: _ventana = instancia de la ventana, width = ancho nuevo, height = alto nuevo
+void Ventana::redimensionar(GLFWwindow * _ventana, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+//USO: revisa los inputs para ponerlos como pulsados o no pulsados
+void Ventana::procesarInputs(GLFWwindow * _ventana)
+{
+    if(glfwGetKey(_ventana, GLFW_KEY_ESCAPE) == GLFW_PRESS)  //Si pulsas Escape
+    {
+        glfwSetWindowShouldClose(_ventana, true);            //Cerrar GLFW
+    }
 }
