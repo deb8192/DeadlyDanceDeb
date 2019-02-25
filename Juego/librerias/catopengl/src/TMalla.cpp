8721a115891cf355576337805cd17c48f15ffb90
josevicenteantoncoy <josevicenteantoncoy@hotmail.com>
2019-02-25 18:52:15 +0100

    Se termina de implementar version basica camara y malla(faltan luces)

--- Juego/librerias/catopengl/src/TMalla.cpp
+++ Juego/librerias/catopengl/src/TMalla.cpp
@@ -29,7 +29,48 @@ void TMalla::cargarMalla(unsigned short,unsigned short)
 // sobrecarga metodos TEntidad
 void TMalla::beginDraw()
 {
-   objetos->Draw(shader);
+    if(matriz_compartida == nullptr)
+    {
+        glm::mat4 * _matriz_resultado = nullptr;
+        std::queue<glm::mat4 *> * cola_compartidaAuxiliar = new std::queue<glm::mat4 *>; //creamos una cola nueva para ir encolando  los elementos que desencolamos de la cola compartida(se aplicaria parecido con una pila)
+    
+        while(cola_compartida->size() > 0)
+        {
+            glm::mat4 * nodo = cola_compartida->front();
+            cola_compartidaAuxiliar->push(nodo);
+            //cogemos el primer valor de la pila
+            if(_matriz_resultado == nullptr)
+            {
+                _matriz_resultado = new glm::mat4;
+                *_matriz_resultado = *nodo;
+            }
+            else
+            {
+                /*if(cola_compartida->size() == 1)
+                {
+                    std::cout << (*nodo)[3][0] << " " << (*nodo)[3][1] << " " << (*nodo)[3][2] << std::endl;
+                }*/
+
+                *_matriz_resultado = (*_matriz_resultado) * (*nodo);
+            }
+
+            cola_compartida->pop();
+        }
+
+        if(_matriz_resultado != nullptr)
+        {
+            objetos->Draw(shader);
+            shader->setMat4("model", (*_matriz_resultado));
+        }
+    }
+    else
+    {
+        if(matriz_compartida != nullptr)
+        {
+            objetos->Draw(shader);
+            shader->setMat4("model", (*matriz_compartida));
+        }
+    }
 }
 
 void TMalla::endDraw()
