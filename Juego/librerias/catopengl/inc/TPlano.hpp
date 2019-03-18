74cb628112f44b6804dd2d032cb1031929eeff4f
davidramons <davidramonsevilla@gmail.com>
2019-03-18 18:22:29 +0100

    mtl actualizados

--- Juego/librerias/catopengl/inc/TPlano.hpp
+++ Juego/librerias/catopengl/inc/TPlano.hpp
@@ -9,18 +9,27 @@ class TPlano : public TEntidad
 
         bool CargarTextura(const char * _ruta);
         void CargarMalla(unsigned int, unsigned int, float);
+        void UdateMesh();
 
+        GLuint winwidth, winheight;
         // const char * _rutafile;
         // unsigned int px, py;
         // float scaleimage;
+        bool cambios = false;
+        float pixx, pixy;
+        float escalado;
         int width, height, nrComponents;
         unsigned int textureID;
         unsigned int VAO, VBO, EBO; //Datos de render
 
     public:
         // Constructor y destructor
-        TPlano(const char *, unsigned int, unsigned int, float, Shader *);
+        TPlano(const char *, unsigned int, unsigned int, float, Shader *, GLuint, GLuint);
         ~TPlano();
+
+        void setSize(float,float); //ancho largo
+        void setPosition(float,float); //x,y
+        void setScale(float); //escalado
 
         // sobrecarga metodos TEntidad
         void beginDraw();
