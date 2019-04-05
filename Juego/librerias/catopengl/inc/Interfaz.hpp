7b234cfa1c467286a1b09ad148ff24e1a71b0076
debora <dgg47@alu.ua.es>
2019-04-05 14:35:52 +0200

    cambio de camara arreglado

--- Juego/librerias/catopengl/inc/Interfaz.hpp
+++ Juego/librerias/catopengl/inc/Interfaz.hpp
@@ -111,6 +111,13 @@ class Interfaz
         void ColorDifusa(unsigned short luz, float r,float g,float b);
         void ColorSpecular(unsigned short luz, float r,float g,float b);
 
+        void setBucle(unsigned short,bool);
+
+        //cercania y lejania de la camara
+        void SetLejaniaCamara(unsigned short camara,float lejania);
+        void SetCercaniaCamara(unsigned short camara,float cercania);
+
+
     private:
 
         bool  banco_ids [65535];
